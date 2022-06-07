9375f50deb67f8063da68c015af96f526045b708
carlosnizolli <carlos.barboza@logcomex.com>
2022-06-07 12:29:52 -0300

    exercicios

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,10 +1,29 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
+    this.beforeEach(function(){
+        cy.visit('./src/index.html')
+    })
     it('verifica o título da aplicação', function() {
-        cy.visit('./scr/index.html')
-
-        cy.title().should('be.equal','Central de Atendimeno ao Cliente TAT')
-
+        cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
+    })
+    it('Preenche os campos obrigatórios e envia o formulário', function(){
+        cy.get('#firstName').type('Carlos')
+        cy.get('#lastName').type('Nizolli')
+        cy.get('#email').type('carlosnizolli@gmail.com')
+        cy.get('#open-text-area').type('Esse é um texto de teste')
+        cy.get('button[type="submit"]').click()
+        cy.get('.success').should('be.visible')
+    })
+    it('Exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function(){
+        cy.get('#firstName').type('Carlos')
+        cy.get('#lastName').type('Nizolli')
+        cy.get('#email').type('carlosnizolli')
+        cy.get('#open-text-area').type('Esse é um texto de teste com erro no e-mail')
+        cy.get('button[type="submit"]').click()
+        cy.get('span[class="error"]').should('be.visible')
+    })
+    it('Campo de telefone só aceita números', function(){
+        cy.get('#phone').type('abc').should('have.value', '')
     })
   })
